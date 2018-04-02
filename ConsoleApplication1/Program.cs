89530e3c6822da512a6cdf1477a61211fcfb04f7
sonphph05134 <sonphph05134@fpt.edu.vn>
2018-04-02 17:27:43 +0700

    up

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine("Hello PT12302_UD");
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
