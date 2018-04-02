2ee2a48114895ae59f4c218fe1ad38f11e35cb9b
sonphph05134 <sonphph05134@fpt.edu.vn>
2018-04-02 18:26:49 +0700

    fix loi

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello PT12302_UD");
+            Console.WriteLine("fix loi");
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
